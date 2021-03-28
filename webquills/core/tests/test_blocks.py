79b7e5639f695bd7af28fb765c2da16d9244d5ab
Vince Veselosky <vince@control-escape.com>
2021-03-28 13:41:29 -0400

    Target id and link icon for heading block

---
+++ webquills/core/tests/test_blocks.py
@@ -0,0 +1,16 @@
+"Tests for custom webquills blocks."
+from webquills.core import blocks
+
+
+def test_heading_block():
+    "Check that a heading block will render"
+    blk = blocks.HeadingBlock()
+    html = blk.render(
+        blk.to_python(
+            {
+                "heading_text": "My Heading",
+                "size": "h2",
+            }
+        )
+    )
+    assert """<h2 id="my-heading">My Heading""" in html
