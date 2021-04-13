a5904e6212e359eb8ac8b4114d82c753b25076fe
Vince Veselosky <vince@control-escape.com>
2021-04-13 13:08:25 -0400

    Autopopulate slugs in admin

--- webquills/core/admin.py
+++ webquills/core/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 
 from webquills.core import models
 
+
+class PageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ["headline"]}
+
+
 # Bare minimum, below customize as needed
-admin.site.register(models.ArticlePage)
+admin.site.register(models.ArticlePage, PageAdmin)
 admin.site.register(models.CallToAction)
-admin.site.register(models.CategoryPage)
+admin.site.register(models.CategoryPage, PageAdmin)
 admin.site.register(models.CopyrightLicense)
-admin.site.register(models.HomePage)
+admin.site.register(models.HomePage, PageAdmin)
 admin.site.register(models.Image)
 admin.site.register(models.SiteMeta)
