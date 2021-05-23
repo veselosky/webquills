f9ad04afc0b2e52085737091519aaef68236752f
Vince Veselosky <vince@control-escape.com>
2021-05-23 16:07:11 -0400

    More user friendly admin, with tests

--- webquills/core/admin.py
+++ webquills/core/admin.py
@@ -4,14 +4,155 @@ from webquills.core import models
 
 
 class PageAdmin(admin.ModelAdmin):
+    list_display = ("headline", "site", "published", "status")
+    list_filter = ("site", "status", "author")
     prepopulated_fields = {"slug": ["headline"]}
+
+
+class ArticleAdmin(PageAdmin):
+    fieldsets = (
+        (
+            "Metadata",
+            {
+                "fields": (
+                    "site",
+                    "author",
+                    "status",
+                    "published",
+                    "updated_at",
+                    "category",
+                    "tags",
+                )
+            },
+        ),
+        (
+            "Content",
+            {
+                "fields": (
+                    "seo_description",
+                    "featured_image",
+                    "headline",
+                    "body",
+                    "slug",
+                )
+            },
+        ),
+        (
+            "Overrides",
+            {
+                "classes": ["collapse"],
+                "fields": (
+                    "seo_title",
+                    "custom_byline",
+                    "custom_about",
+                    "credits",
+                    ("custom_copyright_holder", "custom_copyright_license"),
+                    "custom_copyright_notice",
+                    "expired",
+                ),
+            },
+        ),
+    )
+
+
+class CategoryAdmin(PageAdmin):
+    fieldsets = (
+        (
+            "Metadata",
+            {
+                "fields": (
+                    "site",
+                    "author",
+                    "status",
+                    "published",
+                    "updated_at",
+                    "tags",
+                )
+            },
+        ),
+        (
+            "Content",
+            {
+                "fields": (
+                    "seo_description",
+                    "featured_image",
+                    "headline",
+                    "intro",
+                    "slug",
+                )
+            },
+        ),
+        (
+            "Overrides",
+            {
+                "classes": ["collapse"],
+                "fields": (
+                    "seo_title",
+                    "custom_byline",
+                    "custom_about",
+                    "credits",
+                    ("custom_copyright_holder", "custom_copyright_license"),
+                    "custom_copyright_notice",
+                    "expired",
+                ),
+            },
+        ),
+    )
+
+
+class HomePageAdmin(PageAdmin):
+    fieldsets = (
+        (
+            "Metadata",
+            {
+                "fields": (
+                    "site",
+                    "author",
+                    "status",
+                    "published",
+                    "updated_at",
+                    "tags",
+                )
+            },
+        ),
+        (
+            "Content",
+            {
+                "fields": (
+                    "seo_description",
+                    "featured_image",
+                    "headline",
+                    "lead",
+                    "picture",
+                    "action_label",
+                    "target_url",
+                    "slug",
+                )
+            },
+        ),
+        (
+            "Overrides",
+            {
+                "classes": ["collapse"],
+                "fields": (
+                    "seo_title",
+                    "custom_byline",
+                    "custom_about",
+                    "credits",
+                    ("custom_copyright_holder", "custom_copyright_license"),
+                    "custom_copyright_notice",
+                    "expired",
+                ),
+            },
+        ),
+    )
 
 
 # Bare minimum, below customize as needed
 admin.site.register(models.Author)
-admin.site.register(models.ArticlePage, PageAdmin)
-admin.site.register(models.CategoryPage, PageAdmin)
+admin.site.register(models.ArticlePage, ArticleAdmin)
+admin.site.register(models.CategoryPage, CategoryAdmin)
 admin.site.register(models.CopyrightLicense)
-admin.site.register(models.HomePage, PageAdmin)
+admin.site.register(models.HomePage, HomePageAdmin)
 admin.site.register(models.Image)
 admin.site.register(models.SiteMeta)
